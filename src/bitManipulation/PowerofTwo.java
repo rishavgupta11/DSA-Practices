9d4947261a35b33c111cffc26bbd10a248793ce2
Rishabh Gupta <rishav.mh103@gmail.com>
2024-09-26 01:59:33 +0530

    commit

---
+++ src/bitManipulation/PowerofTwo.java
@@ -0,0 +1,11 @@
+package bitManipulation;
+
+public class PowerofTwo {
+    public static boolean isPowerOfTwo(int n) {
+        return (n&(n-1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(16));
+    }
+}
