3f79300c9b1220da99926ea591dab31cac44b8a5
Rishabh Gupta <rishav.mh103@gmail.com>
2024-09-26 02:09:04 +0530

    Update PowerofTwo.java

--- src/bitManipulation/PowerofTwo.java
+++ src/bitManipulation/PowerofTwo.java
@@ -2,7 +2,7 @@ package bitManipulation;
 
 public class PowerofTwo {
     public static boolean isPowerOfTwo(int n) {
-        return (n&(n-1)) == 0;
+        return n > 0 (n&(n-1)) == 0;
     }
 
     public static void main(String[] args) {
